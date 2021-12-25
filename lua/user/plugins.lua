34c05ed54221b7f2dcf01ef6dc71ba0a232ad173
a2n-s <44101798+AntoineStevan@users.noreply.github.com>
2021-12-25 16:08:04 +0100

    Added media file for telescope + lua completion.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
@@ -83,6 +84,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
+  use "nvim-telescope/telescope-media-files.nvim"
 
   -- Treesitter
   use {
